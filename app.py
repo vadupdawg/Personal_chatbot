2196c1f90418a61f8e3a42bc5ac51b007ed1efb3
vadupdawg <niels1214@gmail.com>
2023-07-17 12:05:00 +0200

    Limit update

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request, jsonify
-import openai
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import openai 
 import os
 
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/")
@@ -10,22 +19,46 @@ def index():
     return render_template("index.html")
 
 @app.route('/chat', methods=['POST'])
+@limiter.limit("10 per minute")
 def chat():
     user_message = request.json.get('user_message')
+    system_message = "Je bent de GroeimetAi-Personal chatbot. Je moet je voordoen als een werknemer van GroeimetAi, een bedrijf dat AI-gestuurde chatbots maakt en bedrijven helpt met AI-ontwikkelingen. Hoewel je geen specifieke database van GroeimetAi hebt om te raadplegen, kun je nog steeds algemene informatie verstrekken op basis van je training. Probeer zo goed mogelijk te helpen met vragen over GroeimetAi."
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
         temperature=0.7,
         messages=[
             {
                 "role": "system",
-                "content": "Je bent de GroeimetAi-Personal chatbot. Je moet je voordoen als een werknemer van GroeimetAi, een bedrijf dat AI-gestuurde chatbots maakt en bedrijven helpt met AI-ontwikkelingen. Hoewel je geen specifieke database van GroeimetAi hebt om te raadplegen, kun je nog steeds algemene informatie verstrekken op basis van je training. Probeer zo goed mogelijk te helpen met vragen over GroeimetAi."
+                "content": system_message
             },
             {
                 "role": "user",
                 "content": user_message
             }
         ]
-   )
+    )
+
+    # Get the number of tokens used in the API call
+    total_tokens = response['usage']['total_tokens']
+
+    # Estimate input tokens
+    input_tokens = len(openai.Tokenizer().tokenize(system_message + user_message))
+    output_tokens = total_tokens - input_tokens
+
+    # The cost per token
+    cost_per_input_token = 0.003 / 1000
+    cost_per_output_token = 0.004 / 1000
+
+    # Calculate the cost of the API call
+    cost_of_input = input_tokens * cost_per_input_token
+    cost_of_output = output_tokens * cost_per_output_token
+    total_cost = cost_of_input + cost_of_output
+
+    # Print the token usage and cost to the console
+    print(f"Input tokens: {input_tokens}, Cost of input: ${cost_of_input:.5f}")
+    print(f"Output tokens: {output_tokens}, Cost of output: ${cost_of_output:.5f}")
+    print(f"Total cost of API call: ${total_cost:.5f}")
+
     chatbot_response = response.choices[0].message['content']
     return jsonify({"chatbot_response": chatbot_response})
 
--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -12,88 +12,94 @@ function getTimestamp() {
   const date = new Date();
   return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
 }
- 
+
 async function sendChatRequest(userMessage, signal) {
   const response = await fetch("/chat", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ user_message: userMessage }),
-      signal: signal,
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ user_message: userMessage }),
+    signal: signal,
   });
   return await response.json();
 }
 
-async function askChatBot(userMessage, retryCount = 3) {
-const controller = new AbortController();
-const signal = controller.signal;
-
-setTimeout(() => controller.abort(), 30000); // Dit annuleert de fetch request na 30 seconden
-
-try {
-  $("#typing-indicator").show();
-  startTypingAnimation();
-    const response = await sendChatRequest(userMessage, signal);
-    console.log("Response from the backend:", response);
-    const chatbotResponse = response.chatbot_response;
-    const timestamp = getTimestamp();
-
-    let responseText = chatbotResponse;
-    $("#chat-output").append(`<div class="message bot"><strong>GroeimetAi:</strong> ${responseText}<span class="timestamp-bot" style="float: left;">${timestamp}</span></div>`);
-    $("#chat-output").scrollTop($("#chat-output")[0].scrollHeight);
-} catch (error) {
-    console.error("Error:", error);
-    if (error.name === 'AbortError') {
-        if (retryCount > 0) {
-            console.log(`Retrying... ${retryCount} attempts left.`);
-            await askChatBot(`${userMessage} maak het antwoord zo klein mogelijk`, retryCount - 1);
-        } else {
-            console.log('Failed after 3 attempts');
-            $("#chat-output").append(`<div class="message bot"><strong>GroeimetAi:</strong> Het antwoord op deze vraag is te lang, probeer de vraag kleiner te maken</div>`);
-        }
-    }
-} finally {
-  stopTypingAnimation();
+function handleChatbotResponse(response, timestamp) {
+  let responseText = response.chatbot_response;
+  $("#chat-output").append(`<div class="message bot"><strong>GroeimetAi:</strong> ${responseText}<span class="timestamp-bot" style="float: left;">${timestamp}</span></div>`);
+  $("#chat-output").scrollTop($("#chat-output")[0].scrollHeight);
 }
 
+function handleChatbotError(error, retryCount, userMessage) {
+  console.error("Error:", error);
+  if (error.name === 'AbortError') {
+    if (retryCount > 0) {
+      console.log(`Retrying... ${retryCount} attempts left.`);
+      askChatBot(`${userMessage} maak het antwoord zo klein mogelijk`, retryCount - 1);
+    } else {
+      console.log('Failed after 3 attempts');
+      $("#chat-output").append(`<div class="message bot"><strong>GroeimetAi:</strong> Het antwoord op deze vraag is te lang, probeer de vraag kleiner te maken</div>`);
+    }
+  }
+}
+
+async function askChatBot(userMessage, retryCount = 3) {
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  setTimeout(() => controller.abort(), 30000); // Dit annuleert de fetch request na 30 seconden
+
+  try {
+    $("#typing-indicator").show();
+    startTypingAnimation();
+    const response = await sendChatRequest(userMessage, signal);
+    console.log("Response from the backend:", response);
+    handleChatbotResponse(response, getTimestamp());
+  } catch (error) {
+    handleChatbotError(error, retryCount, userMessage);
+  } finally {
+    stopTypingAnimation();
+  }
 }
 
 let typingAnimation;
 function startTypingAnimation() {
-let dots = "";
-typingAnimation = setInterval(() => {
-  if (dots.length < 3) {
-    dots += ".";
-  } else {
-    dots = "";
-  }
-  $("#typing-indicator").text(`GroeimetAi is typing${dots}`);
-}, 500);
+  let dots = "";
+  typingAnimation = setInterval(() => {
+    if (dots.length < 3) {
+      dots += ".";
+    } else {
+      dots = "";
+    }
+    $("#typing-indicator").text(`GroeimetAi is typing${dots}`);
+  }, 500);
 }
 
 function stopTypingAnimation() {
-clearInterval(typingAnimation);
-$("#typing-indicator").hide();
+  clearInterval(typingAnimation);
+  $("#typing-indicator").hide();
 }
 
 function sendMessage() {
-const userMessage = $("#input-message").val();
-if (userMessage.trim().length > 0) {
-  const timestamp = getTimestamp();
-  $("#chat-output").append(`<div class="message user"><strong>You:</strong> ${userMessage}<span class="timestamp-user" style="float: right;">${timestamp}</span></div>`);
-  $("#input-message").val("");
-  var userInput = document.getElementById("input-message");
-  userInput.disabled = true;
-  setTimeout(function () {
-    userInput.disabled = false;
-    userInput.focus();
-  }, 2000);
+  const userMessage = $("#input-message").val();
+  if (userMessage.trim().length > 0) {
+    const timestamp = getTimestamp();
+    $("#chat-output").append(`<div class="message user"><strong>You:</strong> ${userMessage}<span class="timestamp-user" style="float: right;">${timestamp}</span></div>`);
+    $("#input-message").val("");
+    $("#char-counter").text("0/140 karakters"); // Deze lijn reset de teller
+    var userInput = document.getElementById("input-message");
+    userInput.disabled = true;
+    setTimeout(function () {
+      userInput.disabled = false;
+      userInput.focus();
+    }, 2000);
 
-  // Scroll naar beneden na het versturen van een bericht
-  $("#chat-output").scrollTop($("#chat-output")[0].scrollHeight);
+    // Scroll naar beneden na het versturen van een bericht
+    $("#chat-output").scrollTop($("#chat-output")[0].scrollHeight);
 
-  askChatBot(userMessage);
-}  
+    askChatBot(userMessage);
+  }  
 }
+
 
 function changePlaceholder() {
   const inputField = $("#input-message");
@@ -106,6 +112,13 @@ function changePlaceholder() {
   }
 }
 
+  // Beperking van de tekstinvoerlengte en de tekensteller
+  $("#input-message").attr("maxlength", 140);
+  $("#input-message").on("input", function () {
+    const currentLength = $(this).val().length;
+    $("#char-counter").text(currentLength + "/140 karakters");
+  });
+
 setInterval(changePlaceholder, 5000);
 
 $(document).ready(async function () {
@@ -117,4 +130,6 @@ $(document).ready(async function () {
   const chatbotResponse = jsonResponse.chatbot_response;
 
   $("#chat-output").append(`<div class="message bot"><strong>GroeimetAi:</strong> ${chatbotResponse}<span class="timestamp-bot" style="float: left;">${timestamp}</span></div>`);
+
+
 });
